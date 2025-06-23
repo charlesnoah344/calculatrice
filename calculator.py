5e8f7303fa9f74bf558aeb3b88d0ff7e23232f41
charlesnoah344 <23397@ecam.be>
2025-06-23 11:31:32 +0200

    first commit

---
+++ calculator.py
@@ -0,0 +1,243 @@
+import pygame
+import pygame_gui
+from pygame_gui.elements import UIButton, UITextEntryLine, UILabel
+import sys
+
+class App:
+  def __init__(self):
+    pygame.init()
+    self.size = (800, 400)
+    self.screen = pygame.display.set_mode(self.size)
+    self.manager = pygame_gui.UIManager(self.size)
+  
+    #déclaration de mes boutons
+    self.one_button = UIButton(
+      relative_rect=pygame.Rect(5, 5+170, 150, 50),
+      text='1',
+      manager=self.manager
+    )
+    self.two_button = UIButton(
+      relative_rect=pygame.Rect(160, 5+170, 150, 50),
+      text='2',
+      manager=self.manager
+    )
+    self.three_button = UIButton(
+      relative_rect=pygame.Rect(315, 5+170, 150, 50),
+      text='3',
+      manager=self.manager
+    )
+    self.four_button = UIButton(
+      relative_rect=pygame.Rect(5, 60+170, 150, 50),
+      text='4',
+      manager=self.manager
+    )
+    self.five_button = UIButton(
+      relative_rect=pygame.Rect(160, 60+170, 150, 50),
+      text='5',
+      manager=self.manager
+    )
+    self.six_button = UIButton(
+      relative_rect=pygame.Rect(315, 60+170, 150, 50),
+      text='6',
+      manager=self.manager
+    )
+    self.seven_button = UIButton(
+      relative_rect=pygame.Rect(5, 115+170, 150, 50),
+      text='7',
+      manager=self.manager
+    )
+    self.eight_button = UIButton(
+      relative_rect=pygame.Rect(160, 115+170, 150, 50),
+      text='8',
+      manager=self.manager
+    )
+    self.nine_button = UIButton(
+      relative_rect=pygame.Rect(315, 115+170, 150, 50),
+      text='9',
+      manager=self.manager
+    )
+    self.point_button = UIButton(
+      relative_rect=pygame.Rect(5, 170+170, 150, 50),
+      text='.',
+      manager=self.manager
+    )
+    self.zero_button = UIButton(
+      relative_rect=pygame.Rect(160, 170+170, 150, 50),
+      text='0',
+      manager=self.manager
+    )
+    self.erase_button = UIButton(
+      relative_rect=pygame.Rect(315, 170+170, 150, 50),
+      text='<<',
+      manager=self.manager
+    )
+    self.plus_button = UIButton(
+      relative_rect=pygame.Rect(470, 5+170, 150, 50),
+      text='+',
+      manager=self.manager
+    )
+    self.minus_button = UIButton(
+      relative_rect=pygame.Rect(470, 60+170, 150, 50),
+      text='-',
+      manager=self.manager
+    )
+    self.multi_button = UIButton(
+      relative_rect=pygame.Rect(470, 115+170, 150, 50),
+      text='*',
+      manager=self.manager
+    )
+    self.divide_button = UIButton(
+      relative_rect=pygame.Rect(470, 170+170, 150, 50),
+      text='/',
+      manager=self.manager
+    )
+    self.egal_button = UIButton(
+      relative_rect=pygame.Rect(625, 5+170, 150, 50),
+      text='=',
+      manager=self.manager
+    )
+    
+    self.display = UILabel(
+      relative_rect=pygame.Rect(350, 50, 150, 50),
+      text='',
+      manager=self.manager
+      )#c'est ce que j'affiche
+    self.number=''
+
+  def process_events(self, event: pygame.event.Event):
+    
+    if event.type == pygame_gui.UI_BUTTON_PRESSED:
+        if event.ui_element is self.one_button:
+            name = self.one_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.two_button:
+            name = self.two_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.three_button:
+            name = self.three_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.four_button:
+            name = self.four_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.five_button:
+            name = self.five_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.six_button:
+            name = self.six_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.seven_button:
+            name = self.seven_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.eight_button:
+            name = self.eight_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.nine_button:
+            name = self.nine_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.zero_button:
+            name = self.zero_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.point_button:
+            if '.' in self.number:
+               for k in range(len(self.number)):
+                  if self.number[k]=='.':
+                     if self.number[k+1]=='.':
+                        pass
+                     else:
+                        name = self.point_button.text
+                        self.number+=name
+                        self.display.set_text(f'{self.number}')
+                   
+            else:
+               name = self.point_button.text
+               self.number+=name
+               self.display.set_text(f'{self.number}')
+               
+        if event.ui_element is self.erase_button:
+           self.number=self.number[:-1]
+           self.display.set_text(f'{self.number}')
+        if event.ui_element is self.plus_button:
+            name = self.plus_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.minus_button:
+            name = self.minus_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.multi_button:
+            name = self.multi_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.divide_button:
+            name = self.divide_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+        if event.ui_element is self.egal_button:
+            name = self.egal_button.text
+            self.number+=name
+            self.display.set_text(f'{self.number}')
+            L=['+','-','*','/' ]
+            for i in range(4):
+               if L[i] in self.number:
+                  if i==0:
+                    for j in range(len(self.number)):
+                        if L[i]==self.number[j]:
+                           m1=float(self.number[:j])+float(self.number[j+1:len(self.number)-1])
+                    self.number+=str(m1)
+                    
+                  if i==1:
+                     for j in range(len(self.number)):
+                        if L[i]==self.number[j]:
+                           m1=float(self.number[:j])-float(self.number[j+1:len(self.number)-1])
+                     self.number+=str(m1)
+
+                  if i==2:
+                     for j in range(len(self.number)):
+                        if L[i]==self.number[j]:
+                           m1=float(self.number[:j])*float(self.number[j+1:len(self.number)-1])
+                     self.number+=str(m1)
+
+                  if i==3:
+                     for j in range(len(self.number)):
+                        if L[i]==self.number[j]:
+                           if self.number[j+1:len(self.number)-1]=='0':
+                              self.number='impossible'
+                            
+                           else:            
+                            m1=float(self.number[:j])/float(self.number[j+1:len(self.number)-1])
+                            self.number+=str(m1)
+
+            self.display.set_text(f'{self.number}')
+                                      
+
+  def run(self):
+    clock = pygame.time.Clock()
+    while True:
+      time_delta = clock.tick(60)
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          sys.exit()
+        if not self.manager.process_events(event):
+          self.process_events(event)
+
+      
+      self.manager.update(time_delta/1000)
+
+      pygame.draw.rect(self.screen, (0, 0, 0), pygame.Rect(0, 0, 800, 600))
+     
+      self.manager.draw_ui(self.screen)
+
+      pygame.display.flip()
+
+
+App().run()
